035b616adba2310a6f55b4bf1290037e0b86dc12
Herman <nitrowitty@gmail.com>
2023-10-21 21:45:05 -0600

    js commandas

--- lib/live_view_studio_web/live/shop_live.ex
+++ lib/live_view_studio_web/live/shop_live.ex
@@ -12,13 +12,36 @@ defmodule LiveViewStudioWeb.ShopLive do
      )}
   end
 
-  def handle_event("toggle-show-cart", _, socket) do
-    socket = update(socket, :show_cart, fn show -> !show end)
-    {:noreply, socket}
-  end
-
   def handle_event("add-product", %{"product" => product}, socket) do
     cart = Map.update(socket.assigns.cart, product, 1, &(&1 + 1))
     {:noreply, assign(socket, :cart, cart)}
+  end
+
+  def toggle_cart(js \\ %JS{}) do
+    js
+    |> JS.toggle(
+      to: "#cart",
+      in: {
+        "ease-in-out duration-300",
+        "translate-x-full",
+        "translate-x-0"
+      },
+      out: {
+        "ease-in-out duration-300",
+        "translate-x-0",
+        "translate-x-full"
+      },
+      time: 300
+    )
+    |> JS.toggle(
+      to: "#backdrop",
+      in: "fade-in",
+      out: "fade-out"
+    )
+  end
+
+  def add_product(product) do
+    JS.push("add-product", value: %{product: product.image})
+    |> JS.transition("shake", to: "#cart-button", time: 500)
   end
 end
